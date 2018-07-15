42d965d4717d1d63f3e187f72442050de44a16ca
aubreeabril <aubree.abril@gmail.com>
2018-07-15 15:56:27 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(category, trait), new_list|
+    trait.each do |value, names|
+      names.each do |name|
+        new_list[name] ||= {}
+        new_list[name][category] ||= []
+        new_list[name][category] << value.to_s
+      end
+    end
+  end
 end
